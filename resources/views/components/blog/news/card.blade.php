fd6b285ec8a3ee8f5767c00bff435eb0e07cff60
mysticthrive <thrivemystic@gmail.com>
2025-04-16 11:59:07 -0300

    update: rearragen icons folder

--- resources/views/components/blog/news/card.blade.php
+++ resources/views/components/blog/news/card.blade.php
@@ -1,9 +1,9 @@
 <x-ui.panel>
   <h3 class="text-xl font-bold">Complete Web Development Roadmap 2024</h3>
   <div class="text-sm font-medium text-gray-600 flex gap-x-2 items-center">
-    <img src="{{ asset('icons/calendar-outline.svg') }}" class="w-4 h-4" alt="">
+    <img src="{{ asset('images/icons/calendar-outline.svg') }}" class="w-4 h-4" alt="">
     <span>12 de Maio, 2025</span>
     <span>·</span>
     <span>Business Tech</span>
-  </div> 
+  </div>
 </x-ui.panel>
--- resources/views/components/blog/resource/card.blade.php
+++ resources/views/components/blog/resource/card.blade.php
@@ -1,6 +1,6 @@
 <x-ui.panel>
   <div class="flex items-center justify-between">
-    <img src="{{ asset('icons/download-outline.svg') }}" class="w-7 h-7" alt="">
+    <img src="{{ asset('images/icons/download-outline.svg') }}" class="w-7 h-7" alt="">
     <x-ui.badge small :pill="false">Resource kit</x-ui.badge>
   </div>
   <div class="space-y-2">
@@ -8,6 +8,6 @@
     <p class="text-sm text-gray-500">Essential tools and resources for modern DevOps practices</p>
   </div>
   <a href="#" class="font-medium text-blue-600 flex items-center gap-2">
-    Download now <img src="{{ asset('icons/download-outline.svg') }}" class="w-5 h-5" alt="">
+    Download now <img src="{{ asset('images/icons/download-outline.svg') }}" class="w-5 h-5" alt="">
   </a>
 </x-ui.panel>
