ba30e8fe157999a9bac70d0cf2d4d60afb76033b
mysticthrive <thrivemystic@gmail.com>
2025-04-19 12:04:44 -0300

    feat: create index post controller route

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+  public function index()
+  {
+    $recentPosts = Post::latest()->take(6)->get();
+    $mostReadPosts = Post::orderBy('views', 'DESC')->take(4)->get();
+
+    return view('posts.index', [
+      'recentPosts' => $recentPosts,
+      'mostReadPosts' => $mostReadPosts
+    ]);
+  }
 }
