bfcbc6adad7a8316487eac36588529906c9db47e
mysticthrive <thrivemystic@gmail.com>
2025-04-22 17:18:24 -0300

    feat: add post and user relation

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,13 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Post extends Model
 {
   /** @use HasFactory<\Database\Factories\PostFactory> */
   use HasFactory;
 
-  public function User(){
+  public function User(): BelongsTo
+  {
     return $this->belongsTo(User::class);
+  }
+
+  public function Tags(): BelongsToMany
+  {
+    return $this->belongsToMany(Tag::class);
   }
 }
