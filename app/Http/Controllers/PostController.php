b3301a14b2e5fe679f99e62d86db3e2d2633cd1b
mysticthrive <thrivemystic@gmail.com>
2025-04-25 16:06:57 -0300

    update: disable lazy loading

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,14 +10,22 @@ class PostController extends Controller
 {
   public function index()
   {
-    $recentPosts = Post::latest()->take(6)->get();
-    $mostReadPosts = Post::orderBy('views', 'DESC')->take(4)->get();
+    $recentPosts = Post::latest()
+      ->take(6)
+      ->with(['tags', 'category', 'user'])
+      ->get();
+
+    $mostReadPosts = Post::orderBy('views', 'DESC')
+      ->take(4)
+      ->with(['tags', 'category', 'user'])
+      ->get();
+      
     $tags = Tag::all();
 
-    return view('posts.index', [
-      'recentPosts' => $recentPosts,
-      'mostReadPosts' => $mostReadPosts,
-      'tags' => $tags
-    ]);
+    return view('posts.index', compact('recentPosts', 'mostReadPosts', 'tags'));
+  }
+
+  public function show(Post $post){
+    return view('posts.show', compact('post'));
   }
 }
