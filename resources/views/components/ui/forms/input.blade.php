5f8cb9b5f379215a6b4e03aa248de6f1613dddab
mysticthrive <thrivemystic@gmail.com>
2025-04-28 17:35:33 -0300

    update: input component refactor

--- resources/views/components/ui/forms/input.blade.php
+++ resources/views/components/ui/forms/input.blade.php
@@ -1,18 +1,40 @@
-@props(['label', 'name', 'icon' => false])
+@props([
+  'label' => false,
+  'icon' => false,
+  'name', 
+  'variant' => 'default',
+  'type' => 'text',
+  'id' => $name,
+  'wrapperClass' => '',
+  'inputClass' => ''
+])
 
 @php
-    $classes = 'w-full pr-3 h-12 border border-blue-300 border-brand-200 rounded-md bg-white focus:border-brand-500 focus:ring-1 focus:ring-brand-500 outline-none transition-colors';
+    $baseClasses = 'w-full pl-3 pr-3 h-12 border rounded-md outline-none transition-colors';
 
-    $classes .= $icon ? ' pl-10' : ' pl-3';
+    $variantClasses = [
+      'default' => 'border-gray-400 focus:border-gray-500 focus:ring-1 focus:ring-gray-500',
+      'blue' => 'border-blue-300 focus:border-blue-500 focus:ring-1 focus:ring-blue-500',
+    ][$variant];
 
-    $defaults = [
-      'type' => 'text',
-      'id' => $name,
-      'name' => $name,
-      'class' => $classes
-    ]
+    $iconPadding = $icon ? ' pl-10' : '';
+
+    $inputClasses = implode(' ', [
+        $baseClasses,
+        $variantClasses,
+        $iconPadding
+    ]);
 @endphp
 
-<x-ui.forms.field :$icon>
-  <input {{ $attributes($defaults) }}/>
+<x-ui.forms.field 
+  :$icon 
+  :$label
+  :$name
+>
+  <input 
+    type="{{ $type }}"
+    id="{{ $id }}"
+    name="{{ $name }}"
+    {{ $attributes->merge(['class' => $inputClasses]) }}
+  />
 </x-ui.forms.field>
