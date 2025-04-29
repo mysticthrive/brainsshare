73ee7dca8ad120d03810c5bb6dc8e4c3d6539457
mysticthrive <thrivemystic@gmail.com>
2025-04-29 12:58:06 -0300

    update: refactor input component to accept textarea

--- resources/views/components/ui/forms/input.blade.php
+++ resources/views/components/ui/forms/input.blade.php
@@ -1,40 +1,45 @@
 @props([
   'label' => false,
   'icon' => false,
-  'name', 
+  'name',
   'variant' => 'default',
   'type' => 'text',
   'id' => $name,
-  'wrapperClass' => '',
-  'inputClass' => ''
+  'as' => 'input'
 ])
 
 @php
-    $baseClasses = 'w-full pl-3 pr-3 h-12 border rounded-md outline-none transition-colors';
+  $baseClasses = 'w-full px-3 py-3 border rounded-md outline-none bg-white transition-colors';
 
-    $variantClasses = [
-      'default' => 'border-gray-400 focus:border-gray-500 focus:ring-1 focus:ring-gray-500',
-      'blue' => 'border-blue-300 focus:border-blue-500 focus:ring-1 focus:ring-blue-500',
-    ][$variant];
+  $variantClasses = [
+    'default' => 'border-gray-300 focus:border-gray-500 focus:ring-1 focus:ring-gray-500',
+    'blue' => 'border-blue-300 focus:border-blue-500 focus:ring-1 focus:ring-blue-500',
+  ][$variant];
 
-    $iconPadding = $icon ? ' pl-10' : '';
+  $iconPadding = $icon ? ' pl-10' : '';
 
-    $inputClasses = implode(' ', [
-        $baseClasses,
-        $variantClasses,
-        $iconPadding
-    ]);
+  $inputClasses = implode(' ', [
+    $baseClasses,
+    $variantClasses,
+    $iconPadding
+  ]);
 @endphp
 
 <x-ui.forms.field 
   :$icon 
-  :$label
+  :$label 
   :$name
 >
-  <input 
-    type="{{ $type }}"
-    id="{{ $id }}"
-    name="{{ $name }}"
-    {{ $attributes->merge(['class' => $inputClasses]) }}
-  />
+  @if ($as === 'textarea')
+    <textarea 
+      id="{{ $id }}"
+      name="{{ $name }}"
+      {{ $attributes->merge(['class' => $inputClasses]) }}></textarea>
+  @else
+    <input 
+      type="{{ $type }}"
+      id="{{ $id }}"
+      name="{{ $name }}"
+      {{ $attributes->merge(['class' => $inputClasses]) }} />
+  @endif
 </x-ui.forms.field>
