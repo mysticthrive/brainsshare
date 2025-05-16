debe41f50d6f6ee1820ea48abc8821c975d61d2f
mysticthrive <thrivemystic@gmail.com>
2025-05-16 17:00:50 -0300

    feat: base dashboard view created

--- resources/views/components/ui/tab.blade.php
+++ resources/views/components/ui/tab.blade.php
@@ -5,7 +5,7 @@
     'bg-white text-gray-900 rounded': {{ $attributes->get('x-model') }} === '{{ $value }}',
     'text-gray-700': {{ $attributes->get('x-model') }} !== '{{ $value }}'
   }"
-  class="py-2 text-sm font-medium text-center flex-1 cursor-pointer"
+  class="px-5 py-2 text-sm font-bold text-center flex-1 cursor-pointer"
   @click="{{ $attributes->get('x-model') }} = '{{ $value }}'"
 >
   {{ $slot }}
