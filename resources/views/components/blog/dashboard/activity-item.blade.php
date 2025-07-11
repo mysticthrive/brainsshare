6dd47b69682ef6ec1b5345f152b4de9e49824b07
mysticthrive <thrivemystic@gmail.com>
2025-07-11 16:51:20 -0300

    update: change icons colors by type of action

--- resources/views/components/blog/dashboard/activity-item.blade.php
+++ resources/views/components/blog/dashboard/activity-item.blade.php
@@ -1,22 +1,22 @@
 @props(['activity'])
 
 @php
-  $icons = [
-    'Post publicado' => 'doc-blue.svg',
-    'Post atualizado' => 'edit-blue.svg',
-    'Post deletado' => 'trash-blue.svg  ',
+  $map = [
+    'Post publicado' => ['icon' => 'new-doc-green.svg', 'bg' => 'bg-green-100'],
+    'Post atualizado' => ['icon' => 'edit-blue.svg', 'bg' => 'bg-blue-100'],
+    'Post deletado'   => ['icon' => 'trash-red.svg', 'bg' => 'bg-red-100'],
   ];
 
-  $icon = $icons[$activity->action];
+  $data = $map[$activity->action] ?? ['icon' => 'doc-blue.svg', 'bg' => 'bg-gray-100'];
 @endphp
 
 <div class="flex items-center gap-3">
-  <div class="w-8 p-2 rounded-full bg-blue-100">
-    <img src="{{ asset('images/icons/' . $icon) }}" alt="">
+  <div class="w-8 p-2 rounded-full {{ $data['bg'] }}">
+    <img src="{{ asset('images/icons/' . $data['icon']) }}" alt="">
   </div>
   
   <div class="flex-1 min-w-0">
-    <h4 class="text-sm font-bold text-gray-800"><a href="">{{ $activity->action }}</a></h4>
+    <h4 class="text-sm font-bold text-gray-800"><a href="#">{{ $activity->action }}</a></h4>
     <p class="text-sm truncate whitespace-nowrap overflow-hidden text-gray-700">
       {{ $activity->description }}
     </p>
